4a1df96b185b135c4c9980499ea6a6ccfcfe3997
Aaron <aaronkao88@gmail.com>
2022-02-26 14:37:01 +0800

    FIX: Deploy Port & BaseUrl

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -374,7 +374,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://api.mdcstudio.tw/',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
