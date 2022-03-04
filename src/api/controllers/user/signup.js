0173b977c7531ed36ecb36f83a30e93a956421ca
shinningyu55688 <shinningyu55688@gmail.com>
2022-03-04 14:59:26 +0800

    FIX: recaptcha secret now stored in .env

--- src/api/controllers/user/signup.js
+++ src/api/controllers/user/signup.js
@@ -11,7 +11,7 @@ module.exports = {
     
     user: {type: "string", required: true }, // email
     password: {type: "string", required: true, encrypt: true} , // 密碼
-    phone: {type: "number", required: true }, // 電話
+    phone: {type: "string", required: true }, // 電話
     name: {type: "string", required: true }, // 名字
 
   },
@@ -34,7 +34,7 @@ module.exports = {
       user: inputs.user
     });
     if (_uf) {
-      return exits.err(100);
+      return exits.err(100); //已存在，回傳
     }
 
     // 建立一筆使用者資料
@@ -42,7 +42,7 @@ module.exports = {
       user: inputs.user,
       password: inputs.password,
       phone: inputs.phone,
-      name: inputs.name
+      name: inputs.name,
     }
 
     const _create = await User.create(data).fetch();
