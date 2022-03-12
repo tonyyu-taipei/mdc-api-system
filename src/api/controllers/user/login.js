1d1eff9f6a05d49d8543448e2797a408e03ba4fc
shinningyu55688 <shinningyu55688@gmail.com>
2022-03-12 16:27:54 +0800

    ADD: Captcha turned off if NODE_ENV != production

--- src/api/controllers/user/login.js
+++ src/api/controllers/user/login.js
@@ -14,7 +14,7 @@ module.exports = {
     
     user: {type: "string", required: true, isNotEmptyString: true }, // email
     password: {type: "string", required: true, isNotEmptyString: true} , // 密碼
-    recaptcha: {type: "string", required: true, isNotEmptyString: true}
+    recaptcha: {type: "string", required: true, isNotEmptyString: true} //recaptcha驗證碼，開發模式下只需有數值即可
   },
 
 
@@ -34,7 +34,7 @@ module.exports = {
       // user: exits.user,
       // password: exits.password
       where: {user: inputs.user},
-      select: ['user', 'password']
+      select: ['user', 'password','name']
     }).decrypt();
     
     if (!_u) {
@@ -46,20 +46,31 @@ module.exports = {
       return exits.err(102);
     }
 
-    // Recaptcha 認證
-    const _captcha = await axios({
-      method: 'POST',
-      url:'https://www.google.com/recaptcha/api/siteverify',
-      data: qs.stringify({
-        secret: process.env.recaptcha,
-        response: inputs.recaptcha
-    })
-      }).then(res=>res)
-      if(!_captcha.data.success)
-      return exits.err(105)
+
+    // Recaptcha 認證（在開發模式下不進行驗證）
+    if(process.env.NODE_ENV =="production"){
+
+        
+        const _captcha = await axios({
+          method: 'POST',
+          url:'https://www.google.com/recaptcha/api/siteverify',
+          data: qs.stringify({
+            secret: process.env.recaptcha,
+            response: inputs.recaptcha
+        })
+          }).then(res=>res)
+          if(!_captcha.data.success)
+          return exits.err(105)
+
+    }
     // 如果正確後生產Token
     await User_session.destroy({});
     const _session = await User_session.create({}).fetch();
+
+    this.req.session.user = {
+        user: _u.user,
+        name: _u.name
+    };
 
     // All done.
     return exits.success({
