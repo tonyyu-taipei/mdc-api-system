df7f69611305b34f030588a2d897755c050232af
shinningyu55688 <shinningyu55688@gmail.com>
2022-04-01 16:32:48 +0800

    FIX: cart bugs

--- src/api/controllers/cart/find.js
+++ src/api/controllers/cart/find.js
@@ -29,7 +29,21 @@ module.exports={
         if(this.req.session.dateRange && this.req.session.cart)
         this.req.session.cart.price = await pricecalc(this.req.session.cart.items, Math.abs(differenceInDays(new Date(this.req.session.dateRange[0]),new Date(this.req.session.dateRange[1]) )))
 
-        return exits.success(this.req.session.cart);
+        try{
+          this.req.session.dateRange[0]&& !this.req.session.dateRange[1]
+        }
+        catch(err){
+          return exits.success({
+            ...this.req.session.cart,
+            msg:"Please make sure you have a valid date range selected",
+            msgCH:"請確認您是否已經選取正確的借用日期範圍"
+          })        
+        }
+        return exits.success({
+          ...this.req.session.cart,
+          msg:"Item added.",
+          msgCH:"商品已加入清單"
+        })        ;
 
   
     }
