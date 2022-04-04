dc44a675f2a9d64411945c4a9d80893b767026cd
shinningyu55688 <shinningyu55688@gmail.com>
2022-04-04 11:07:19 +0800

    ADD: comments

--- src/api/controllers/cart/destroy.js
+++ src/api/controllers/cart/destroy.js
@@ -4,12 +4,12 @@ module.exports = {
   friendlyName: 'Destroy',
 
 
-  description: 'Destroy cart.',
+  description: 'Splice the cart.',
 
 
   inputs: {
 
-    id: {type: 'number' , required: true},
+    id: {type: 'number' , required: true, description:"the id that you wanna remove from the cart"},
   },
 
 
@@ -27,13 +27,16 @@ module.exports = {
 
     let requestedId = inputs.id;
     
+    // get the cart session
     let requestArr = this.req.session.cart.items;
 
     let index = requestArr.indexOf(requestedId);
 
+    //check if id exists in cart
     if(index == -1){
         exits.err(700)
     }else{
+       // remove the id from cart
         this.req.session.cart.items.splice(index,1)
         exits.success(this.req.session.cart);
 
