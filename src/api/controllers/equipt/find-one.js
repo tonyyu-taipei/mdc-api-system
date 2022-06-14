c7a22ba196ce041e31c1a46b5df3294a3298ad33
shinningyu55688 <shinningyu55688@gmail.com>
2022-06-14 22:25:33 +0800

    ADD: option to not send the hidden Equipt.

--- src/api/controllers/equipt/find-one.js
+++ src/api/controllers/equipt/find-one.js
@@ -30,8 +30,14 @@ module.exports = {
       id: inputs.id
     });
 
-    
-    if (!_ef || _ef.access >= 2) {
+
+    if (!_ef) {
+      return exits.err(402);
+    }
+
+    //管理員仍能看到所有器材 available 2為隱藏，管理員將看到4為使用者隱藏的器材
+    if(_ef.available == 2){
+      if(!this.req.session.user?.admin)
       return exits.err(402);
     }
     
