819fe047a617e17a49ad8b5868cf48a43dd358aa
shinningyu55688 <shinningyu55688@gmail.com>
2022-07-01 14:05:41 +0800

    FIX: Admin Find Equipt Availability Bug

--- src/api/controllers/equipt/find.js
+++ src/api/controllers/equipt/find.js
@@ -27,13 +27,18 @@ module.exports = {
     // 取得所有器材資料
       let data = await Equipt.find({});
 
-    //管理員仍能看到所有器材 available 2為隱藏，管理員將看到4為使用者隱藏的器材
+    //管理員仍能看到所有器材 available 2為隱藏，管理員將看到4為隱藏的器材
     if(this.req.session.user?.admin){
-      data = data.filter(item =>{
-        return item.available = item.available == 2?4: item.available;
-      }) 
+      let dataTmp = new Array();
+      data.forEach(element=>{
+        if(element.available == 2){
+          element.available = 4;
+          dataTmp.push(element);
+        }
+      })
+      data.concat(dataTmp);
     }
-
+    sails.log("User")
       data = data.filter(data=>{
         return data.available != 2;
       })
