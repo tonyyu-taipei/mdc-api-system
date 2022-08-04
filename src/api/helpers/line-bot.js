206fc1514661dc612588a677ef815ba9c9b644a7
shinningyu55688 <shinningyu55688@gmail.com>
2022-08-04 12:12:22 +0800

    ADD: Don't Send Line While In Dev. Mode

--- src/api/helpers/line-bot.js
+++ src/api/helpers/line-bot.js
@@ -17,11 +17,13 @@ module.exports={
         channelSecret:process.env.LINE_CHSECRET,
         channelAccessToken:process.env.LINE_CHTOKEN
     })
-    let res = await bot.broadcast(`【器材系統訊息推播】\n${inputs.message}`);
-
-    if(!res){
-        return exits.error()
-    }
+//Won't broadcast to Line during dev.
+    if(process.env.NODE_ENV =="production"){
+        let res = await bot.broadcast(`【器材系統訊息推播】\n${inputs.message}`);
+        if(!res){
+            return exits.error()
+        }
+    }  
     return exits.success();
     }
 }
