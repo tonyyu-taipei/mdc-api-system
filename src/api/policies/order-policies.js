134a5518f9e45593bad7be6276268f90e3b64406
shinningyu55688 <shinningyu55688@gmail.com>
2022-08-08 15:26:38 +0800

    ADD: Policies On Unverified User

--- src/api/policies/order-policies.js
+++ src/api/policies/order-policies.js
@@ -1,8 +1,13 @@
 module.exports= async function(req,res,next){
     if(req.session.user){
-        if(req.session.dateRange)
-            if(req.session.cart)
-                return next()
+        let _u = await User.findOne({id: req.session.user.id})
+        if(_u.verified){
+            if(req.session.dateRange)
+                if(req.session.cart)
+                    return next()
+        }else{
+            return res.forbidden("帳戶尚未驗證，請至信箱收取確認信後再試。")
+        }
     }
     return res.forbidden("請檢查是否有登入、日期範圍及購物車是否有資料。");
     
