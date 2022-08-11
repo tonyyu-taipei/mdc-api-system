42bf5859082a77b70fe0c4379b92720fdedaaab0
shinningyu55688 <shinningyu55688@gmail.com>
2022-08-11 22:47:59 +0800

    FIX: Order/Update

--- src/api/controllers/order/update.js
+++ src/api/controllers/order/update.js
@@ -8,20 +8,28 @@ module.exports = {
 
 
   inputs: {			
-    
-    name: { type: 'string', required: true, columnType:'varchar(10)' },  // 取件人名稱	
-    useUD: { type: 'number', required: true , columnType:'int2' },  // 0- true 1- false
-    notes: { type: 'number', required: true , columnType:'int2' },  // 備註		
-    phone: { type: 'string', required: true, columnType:'text' },  // 聯絡電話	
-    status: {type: 'number', required: true , columnType:'int2'},  // 0 -訂單等待管理員確認中 
+    id:{type: 'number', required:true}, // 訂單ID
+    name: { type: 'string',  columnType:'varchar(10)' },  // 取件人名稱	
+    notes: { type: 'number',  columnType:'int2' },  // 備註		
+    phone: { type: 'string',  columnType:'text' },  // 聯絡電話	
+    status: {type: 'number',  columnType:'int2'},  // 0 -訂單等待管理員確認中 
                                                                     // 1 -管理員已經正在聯絡中 
                                                                     // 2-聯絡完成，等待取件借用 
                                                                     // 3-借用中 
                                                                     // 4-已經歸還（訂單完成）
                                                                     // 5-	逾時 
                                                                     // 6-取消 
-                                                                    // 16-於管理員已經正在聯絡中取消 
-   },                                                               // 26-聯絡完成但使用者要求取消
+                                                                    // 16-於管理員已經正在聯絡中取消
+                                                                    // 26-聯絡完成但使用者要求取消
+    contains: {type: 'json', columnType:"array"},
+    bundled: {type:'json', columnType: 'array'},
+    price:{type:'number'},
+    from:{ type:"string"},
+    to:{type:"string"},
+    email:{type:"string"}
+
+   },
+                                                                  
 
   
   exits: {
